b8eefc75da6a1b3366ecd9fd887dc77b99c3cd7e
Argenis Osorio <argenisosorio580@hotmail.com>
2020-04-28 16:20:31 +0000

    Agregada nueva plantilla

---
+++ templates_examples/bootstrap_services_template/README.md
@@ -0,0 +1,3 @@
+# Portafolio del Ing. Argenis Osorio
+
+<a href="https://argenisosorio.github.io/portafolio/">Ver en la web</a>
---
+++ templates_examples/bootstrap_services_template/static/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  /*
+  |--------------------------------------------
+  | Función que permite la nevegación suave 
+  | entre secciones por medio de enlaces ancla.
+  |--------------------------------------------
+  */
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+    return false;
+  });
+});
